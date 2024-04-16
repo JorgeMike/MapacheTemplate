a7c83a582bfa34e8c72ac44ea6ae91213b209fd4
ElMiguelon <alvaradojr2001@gmail.com>
2024-04-16 13:14:22 -0600

    Calendario y graficas

---
+++ Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  MapacheTemplate
+//
+//  Created by CEDAM 13 on 16/04/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
