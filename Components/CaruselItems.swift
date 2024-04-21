2d9ff8f610ef2b2cf5a27d8692bfe034d58664dc
ElMiguelon <alvaradojr2001@gmail.com>
2024-04-20 18:50:14 -0600

    muchos cmabios

---
+++ Components/CaruselItems.swift
@@ -0,0 +1,18 @@
+//
+//  CaruselItems.swift
+//  MapacheTemplate
+//
+//  Created by CEDAM 13 on 20/04/24.
+//
+
+import SwiftUI
+
+struct CaruselItems: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CaruselItems()
+}
---
+++ Views/InventoryView.swift
@@ -0,0 +1,18 @@
+//
+//  InventoryView.swift
+//  MapacheTemplate
+//
+//  Created by CEDAM 13 on 20/04/24.
+//
+
+import SwiftUI
+
+struct InventoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InventoryView()
+}
