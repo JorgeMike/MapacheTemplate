b6200797b21d54995ead03a553f23f31c8900d59
ElMiguelon <alvaradojr2001@gmail.com>
2024-04-21 10:36:53 -0600

    Home page

---
+++ Components/InventoryProgress.swift
@@ -0,0 +1,18 @@
+//
+//  InventoryProgress.swift
+//  MapacheTemplate
+//
+//  Created by CEDAM 13 on 21/04/24.
+//
+
+import SwiftUI
+
+struct InventoryProgress: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InventoryProgress()
+}
---
+++ Components/Percentage.swift
@@ -0,0 +1,18 @@
+//
+//  Percentage.swift
+//  MapacheTemplate
+//
+//  Created by CEDAM 13 on 21/04/24.
+//
+
+import SwiftUI
+
+struct Percentage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Percentage()
+}
