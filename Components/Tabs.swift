8ec8061c2e3b6a62e39cc6e5fa6a721e66fd834e
ElMiguelon <alvaradojr2001@gmail.com>
2024-04-21 15:36:53 -0600

    Graficas de barra y chat IA

---
+++ Components/Tabs.swift
@@ -0,0 +1,18 @@
+//
+//  Tabs.swift
+//  MapacheTemplate
+//
+//  Created by CEDAM 13 on 21/04/24.
+//
+
+import SwiftUI
+
+struct Tabs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tabs()
+}
