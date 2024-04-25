f39b8caa1882c513a7db55682473a96165216e11
ElMiguelon <alvaradojr2001@gmail.com>
2024-04-25 16:50:40 -0600

    Cambios

---
+++ Modals/ProductDetailModal.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailModal.swift
+//  MapacheTemplate
+//
+//  Created by CEDAM 13 on 21/04/24.
+//
+
+import SwiftUI
+
+struct ProductDetailModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailModal()
+}
